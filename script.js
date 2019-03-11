7ca66dd5a5e1ca5c0545bb227434fb7ac0157a0d
Sasha Miller <sashazm85@gmail.com>
2019-03-11 12:57:48 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// To select the button
+const colorBtn = document.querySelector('.colorBtn');
+
+// To select the body for background color change
+const bodyColorize = document.querySelector('body');
+
+// Select the span where hex value will be injected
+const hex = document.querySelector('.hex');
+
+
+// Create a function that generates random colors
+function colors() {
+  const rgb = () => ('0' + (Math.random() * 256).toString(16)).slice(-2);
+  let hexCol = `#${rgb()}${rgb()}${rgb()}`;
+  return hexCol;
+}
+
+//Event listener with changeColor callback function
+colorBtn.addEventListener('click', changeColor);
+
+// changeColor function call
+function changeColor () {
+  bodyColorize.style.backgroundColor = colors();
+  hex.innerHTML = colors("hexCol");
+}
